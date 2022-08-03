bc806016a0c2078a11639b74d67d8fc3cae4802d
akshita1996 <109249436+akshita1996@users.noreply.github.com>
2022-08-03 21:23:52 +0200

    cleanup

---
+++ src/position_service.cpp
@@ -0,0 +1,92 @@
+/**
+* \file position_service.cpp
+* \brief Node random_position_server, which implements a random position service
+* \author Akshita Kadambathil
+* \version 0.1
+* \date 03/08/2022
+*/
+
+#include <chrono>
+#include <memory>
+#include <inttypes.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "rt2_assignment1/srv/random_position.hpp"
+
+/*Register a component that can be dynamically loaded at runtime*/
+#include "rclcpp_components/register_node_macro.hpp"
+
+using namespace std::chrono_literals;
+
+using RandomPosition = rt2_assignment1::srv::RandomPosition;	//name of my .srv file, I'll get the informations from there
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+
+namespace rt2_assignment1{
+/**
+*
+* Description:
+* Generate a random number between M and N (/position_server)
+*/
+
+class Position_srv : public rclcpp::Node
+{
+public:
+/**
+*
+* \param const rclcpp::NodeOptions (options)
+*
+* Description:
+* Run the node (random_position_server) as a component
+*/
+	Position_srv(const rclcpp::NodeOptions & options)
+	: Node("random_position_server", options)
+	{
+		service_ = this->create_service<RandomPosition>(
+      "/position_server", std::bind(&Position_srv::myrandom, this, _1, _2, _3));
+      }
+
+private:
+/**
+*
+* \param double (M)
+* \param double (N)
+*
+* \return double (randMToN)
+*
+* Description:
+* Generate a random number between M and N (/position_server)
+*/
+	double randMToN(double M, double N)
+{     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
+
+/**
+*
+* \param rmw_request_id_t (request_header)
+* \param RandomPosition::Request (req) - service with x and y range
+* \param RandomPosition::Response (res) - service with x, y and theta
+*
+* Description:
+* My service callback that generates the random values for the positions x, y and theta
+*/
+
+void myrandom (
+	const std::shared_ptr<rmw_request_id_t> request_header,
+	const std::shared_ptr<RandomPosition::Request> req, 
+	const std::shared_ptr<RandomPosition::Response> res)
+	{
+	(void)request_header;
+	res->x = randMToN(req->x_min, req->x_max);
+	res->y = randMToN(req->y_min, req->y_max);
+	res->theta = randMToN(-3.14, 3.14);
+	}
+rclcpp::Service<RandomPosition>::SharedPtr service_;  //my server for position
+};
+			}	
+
+RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::Position_srv)
+	
---
+++ src/state_machine.cpp
@@ -0,0 +1,174 @@
+/**
+* \file state_machine.cpp
+* \brief Implements a service to start or stop the robot, and calls the other two services to drive the robot
+* \author Akshita Kadambathil
+* \version 0.1
+* \date 03/08/2022
+*/
+
+#include <chrono>
+#include <memory>
+#include <inttypes.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "rt2_assignment1/srv/random_position.hpp"
+#include "rt2_assignment1/srv/command.hpp"
+#include "rt2_assignment1/srv/position.hpp"
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+using namespace std::chrono_literals;
+
+using Command = rt2_assignment1::srv::Command;	
+using Position = rt2_assignment1::srv::Position;
+using RandomPosition = rt2_assignment1::srv::RandomPosition;	
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;	
+
+namespace rt2_assignment1{
+
+/**
+*
+* Description:
+* Receives the user's request to start moving the mobile robot, the random pose is received by the /position_server and then sent to go_to_point service, the process will repeat in a loop unitl the user send a command for the robot stop
+*/
+class State_Machine : public rclcpp::Node
+{
+public:
+
+/**
+* 
+* \param rclcpp::NodeOptions (options)
+*  run the node as component
+*
+* Description:
+* Initialize the variables, server and 2 clients
+*/ 
+	State_Machine(const rclcpp::NodeOptions & options)
+	: Node("state_machine", options)
+	{
+	start = false;
+	position_reached = true;
+	
+	/*Initialization client and service*/  
+	
+      service_ = this->create_service<Command>(
+      "/user_interface", std::bind(&State_Machine::user_interface, this, _1, _2, _3));  
+      client_rp = this->create_client<RandomPosition>("/position_server");
+      
+      while (!client_rp->wait_for_service(std::chrono::seconds(1))){
+      	if (!rclcpp::ok()) {
+      RCLCPP_ERROR(this->get_logger(), "client interrupted while waiting for service to appear.");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "waiting for service to appear...");
+  }
+  
+  client_p = this->create_client<Position>("/go_to_point");
+  
+  while (!client_p->wait_for_service(std::chrono::seconds(1))){
+     if (!rclcpp::ok()) {
+      RCLCPP_ERROR(this->get_logger(), "client interrupted while waiting for service to appear.");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "waiting for service to appear...");
+  }
+  
+  rp_req = std::make_shared<RandomPosition::Request>();
+  rp_res = std::make_shared<RandomPosition::Response>();
+  p_req = std::make_shared<Position::Request>();
+  
+  rp_req->x_max = 5.0;
+  rp_req->x_min = -5.0;
+  rp_req->y_max = 5.0;
+  rp_req->y_min = -5.0;
+}        
+
+/**
+* 
+* \param rmw_request_id_t (request_header)
+*  Call header
+*
+* \param Command::Request (req)
+*  Service Request with command (string)
+*
+* \param Command::Response (res)
+*  Service Response with the value of the bool (start)
+*
+* Description:
+* Service callback to set the start and stop arguments state
+*/ 
+  void user_interface(
+  	 const std::shared_ptr<rmw_request_id_t> request_header,
+  	 const std::shared_ptr<Command::Request> req,
+  	 const std::shared_ptr<Command::Response> res)
+  	{
+  	(void) request_header;
+  		if (req->command == "start")
+  		{
+  		start = true;
+  		Go_To_Point();
+  		}
+  		else
+  		{
+  		start = false;
+  		}
+  	res->ok = start;
+  	RCLCPP_INFO(this->get_logger(), "Received request %s", req->command.c_str());
+  	}  	
+
+/**
+*
+* Description:
+* Receive and set a new goal position, this position comes from /position_server and go to /go_to_point as a request to set the new goal position for the robot.
+*/
+void Go_To_Point(){
+    
+    myrandom_call();
+    
+    position_reached = false; // receive a new goal
+    
+    p_req->x = rp_res->x;
+    p_req->y = rp_res->y;
+    p_req->theta = rp_res->theta;
+    
+    RCLCPP_INFO(this->get_logger(), "Going to the position: x= %f y= %f theta= %f",
+            p_req->x, p_req->y, p_req->theta);
+    auto point_reached_callback =
+              [this](rclcpp::Client<rt2_assignment1::srv::Position>::SharedFuture future){(void)future; position_reached = true;
+               RCLCPP_INFO(this->get_logger(), "Goal reached!");};
+    auto future_result = client_p->async_send_request(p_req, point_reached_callback);
+  }
+
+/**
+*
+* Description:
+* Function that call my rnaodm position service. The positon of the goal received from the /position_server service is stored in the variable rp_res as a "copy"
+*/
+void myrandom_call(){  
+	auto rp_res_callback = [this](rclcpp::Client<rt2_assignment1::srv::RandomPosition>::SharedFuture future){rp_res = future.get();};
+	auto future_result = client_rp->async_send_request(rp_req, rp_res_callback);
+}
+
+/*int main() part*/
+
+/*global variables*/
+bool start, position_reached;
+
+rclcpp::Service<Command>::SharedPtr service_;
+rclcpp::Client<RandomPosition>::SharedPtr client_rp;
+rclcpp::Client<Position>::SharedPtr client_p;
+
+std::shared_ptr<RandomPosition::Request> rp_req;
+std::shared_ptr<RandomPosition::Response> rp_res;
+std::shared_ptr<Position::Request> p_req;
+
+};
+}
+
+RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::State_Machine)
+
